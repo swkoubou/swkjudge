4306f524ab6234030df39e9b088179dddabefb6c
iga-c <twgiko@gmail.com>
2014-08-11 23:07:13 +0900

    scaffoldでuserテーブルを追加

---
+++ fuel/app/classes/model/user.php
@@ -0,0 +1,46 @@
+<?php
+use Orm\Model;
+
+class Model_User extends Model
+{
+	protected static $_properties = array(
+		'id',
+		'username',
+		'password',
+		'group',
+		'email',
+		'last_login',
+		'login_hash',
+		'profile_fields',
+		'exp',
+		'created_at',
+		'updated_at',
+	);
+
+	protected static $_observers = array(
+		'Orm\Observer_CreatedAt' => array(
+			'events' => array('before_insert'),
+			'mysql_timestamp' => false,
+		),
+		'Orm\Observer_UpdatedAt' => array(
+			'events' => array('before_save'),
+			'mysql_timestamp' => false,
+		),
+	);
+
+	public static function validate($factory)
+	{
+		$val = Validation::forge($factory);
+		$val->add_field('username', 'Username', 'required|max_length[50]');
+		$val->add_field('password', 'Password', 'required|max_length[255]');
+		$val->add_field('group', 'Group', 'required|valid_string[numeric]');
+		$val->add_field('email', 'Email', 'required|valid_email|max_length[255]');
+		$val->add_field('last_login', 'Last Login', 'required|max_length[25]');
+		$val->add_field('login_hash', 'Login Hash', 'required|max_length[255]');
+		$val->add_field('profile_fields', 'Profile Fields', 'required');
+		$val->add_field('exp', 'Exp', 'required|valid_string[numeric]');
+
+		return $val;
+	}
+
+}
---
+++ fuel/app/logs/2014/08/11.php
@@ -0,0 +1,6 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2014-08-11 23:04:23 --> Fuel\Core\Fuel::init - The configured locale ja_JP is not installed on your system.
+WARNING - 2014-08-11 23:04:38 --> Fuel\Core\Fuel::init - The configured locale ja_JP is not installed on your system.
+WARNING - 2014-08-11 23:04:50 --> Fuel\Core\Fuel::init - The configured locale ja_JP is not installed on your system.
+WARNING - 2014-08-11 23:06:16 --> Fuel\Core\Fuel::init - The configured locale ja_JP is not installed on your system.
